695549a76a7416ef5ad9f488b6e456c6caa8ffe3
FeisalAlaswad <154249501+FeisalAlaswad@users.noreply.github.com>
2025-12-17 19:26:31 +0530

    Update prepareData.py

--- prepareData.py
+++ prepareData.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 16 12:50:21 2018
 
-@author: bking
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
